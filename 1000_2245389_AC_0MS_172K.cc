3589b53b6dab3c961ec0311b813bc2e3ffdbb689
zhaofeng37 <zhao.f@hotmail.com>
2018-09-23 15:43:10 +0800

    add 1000_2245389_AC_0MS_172K.cc A+B Problem

---
+++ 1000_2245389_AC_0MS_172K.cc
@@ -0,0 +1,23 @@
+// POJ 1000 A+B Problem
+/**
+This is an entry level problem.
+It will show participant how to use the input system to run and judge the commitment.
+Glimpse and step into the next problem.
+*/
+#include <iostream>
+
+using namespace std;
+
+int  main()
+
+{
+
+    int a,b;
+
+    cin >> a >> b;
+
+    cout << a+b << endl;
+
+    return 0;
+
+}
